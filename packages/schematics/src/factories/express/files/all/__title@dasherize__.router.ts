b8589187aa8cb8b1f8b38fd42acc9158e45d68a0
Peter B Smith <1965474+peterbsmyth@users.noreply.github.com>
2023-06-02 19:04:53 -0700

    1.4.44

---
+++ packages/schematics/src/factories/express/files/all/__title@dasherize__.router.ts
@@ -0,0 +1,91 @@
+import express, { Request, Response } from 'express';
+import { 
+  <%= classify(title) %>,
+  <%= classify(title) %>Writeonly,
+  <%= classify(title) %>Index,
+  <%= underscore(title).toUpperCase() %>,
+  indicies 
+} from '<%= entities %>';
+
+const mn = configure({
+  integrationKey: process.env.INTEGRATION_KEY!,
+  indicies
+});
+
+export const <%= camelize(title) %>Router = express.Router();
+
+<%= camelize(title) %>Router.get('/', async (req: Request, res: Response) => {
+  cons where = req.query.where as MountNotionQueryParameters<<%= classify(title) %>Index>;
+  try {
+    const <%= camelize(title) %>: <%= classify(title) %>[] = await mn.<%= camelize(title) %>.query(where);
+    
+
+    res.status(200).send(<%= camelize(title) %>);
+  } catch (e) {
+    res.status(500).send(e.message);
+  }
+});
+
+<%= camelize(title) %>Router.get('/:id', async (req: Request, res: Response) => {
+  const id: number = parseInt(req.params.id, 10);
+
+  try {
+    const item: <%= classify(title) %> = await mn.<%= camelize(title) %>.retrieve({ id });
+
+    if (item) {
+      return res.status(200).send(item);
+    }
+
+    res.status(404).send('item not found');
+  } catch (e) {
+    res.status(500).send(e.message);
+  }
+});
+
+<%= camelize(title) %>Router.post('/', async (req: Request, res: Response) => {
+  try {
+    const item: Partial<<%= classify(title) %>Writeonly> = req.body;
+    const name = uniqueNamesGenerator({
+      dictionaries: [animals, colors],
+      separator: '-',
+      length: 2,
+    });
+
+    const new<%= classify(title) %> = await mn.<%= camelize(title) %>.create({
+      name,
+      ...body,
+    });
+
+    res.status(201).json(new<%= classify(title) %>);
+  } catch (e) {
+    res.status(500).send(e.message);
+  }
+});
+
+<%= camelize(title) %>Router.patch('/:id', async (req: Request, res: Response) => {
+  const id: number = parseInt(req.params.id, 10);
+
+  try {
+    const itemUpdate: Partial<<%= classify(title) %>Writeonly> = req.body;
+
+    const updated<%= classify(title) %> = mn.<%= camelize(title) %>.update({
+      id,
+      ...body,
+    });;
+
+    res.status(200).json(updated<%= classify(title) %>);
+  } catch (e) {
+    res.status(500).send(e.message);
+  }
+});
+
+<%= camelize(title) %>Router.delete('/:id', async (req: Request, res: Response) => {
+  try {
+    const id: number = parseInt(req.params.id, 10);
+    const deleted = await mn.delete({ id });
+
+    res.status(204).send(deleted);
+  } catch (e) {
+    res.status(500).send(e.message);
+  }
+});
--- packages/schematics/src/factories/express/files/index/index.ts
+++ packages/schematics/src/factories/express/files/index/index.ts
@@ -1,3 +1,30 @@
+import 'dotenv/config';
+
+import express from 'express';
+import cors from 'cors';
+import helmet from 'helmet';
+import * as path from 'path';
+
 <% for(const title of titles) { %>
-  export * from './<%= dasherize(title) %>.controller';
+  import { <%= dasherize(title) %>Router } from './<%= dasherize(title) %>.router';
 <% } %>
+
+
+
+
+const PORT: number = process.env.PORT ? parseInt(process.env.PORT as string, 10): 3000;
+const app = express();
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
+
+<% for(const title of titles) { %>
+  app.use('/<%= dasherize(title) %>', <%= dasherize(title) %>Router);
+  import * from './<%= dasherize(title) %>.router';
+<% } %>
+
+const server = app.listen(PORT, () => {
+  console.log(`listening at http://localhost:${PORT}`);
+});
+
+server.on('error', console.error);
