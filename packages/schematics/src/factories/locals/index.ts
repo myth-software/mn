92be66d93011f02f3e53cf0c6244c413c799883b
Peter B Smith <1965474+peterbsmyth@users.noreply.github.com>
2023-07-01 14:58:35 -0600

    1.4.77

--- packages/schematics/src/factories/locals/index.ts
+++ packages/schematics/src/factories/locals/index.ts
@@ -2,6 +2,7 @@ import { chain, move, Rule, template, url } from '@angular-devkit/schematics';
 import { createDatabaseCaches } from '@mountnotion/sdk';
 import { Cache, LocalsOptions } from '@mountnotion/types';
 import { logSuccess, strings } from '@mountnotion/utils';
+import { rimraf } from 'rimraf';
 import { applyWithOverwrite } from '../../rules';
 import { getlocals } from '../../utils';
 
@@ -19,66 +20,63 @@ export function locals(options: LocalsOptions): Rule {
   let cachesRef: Cache[] = [];
   let titlesRef: string[] = [];
 
-  return () =>
-    createDatabaseCaches(pageIds, options)
-      .then((caches) => {
-        const includedCaches = caches.filter(
-          ({ title }) => title && !excludes.includes(title)
+  return async () => {
+    await rimraf(outDir);
+    const caches = await createDatabaseCaches(pageIds, options);
+    const includedCaches = caches.filter(
+      ({ title }) => title && !excludes.includes(title)
+    );
+    cachesRef = includedCaches;
+    const localsPromises = includedCaches.map((cache) =>
+      getlocals({ ...options, pageId: cache.id })
+    );
+    const locals = await Promise.all(localsPromises);
+    const rules = locals
+      .map(({ title: title_1, locals: locals_1 }) => {
+        titlesRef = cachesRef.map(({ title: title_2 }) => title_2) as string[];
+        const localsRules = locals_1.map((local) => {
+          const { title: localTitle, ...rest } = local;
+          const formattedTitle = strings.titlize(localTitle);
+          return applyWithOverwrite(url('./files/all-for-entity'), [
+            template({
+              title: formattedTitle,
+              local: rest,
+              entities,
+              databaseName: title_1,
+              ...strings,
+            }),
+            move(`${outDir}/${strings.dasherize(title_1)}`),
+          ]);
+        });
+
+        const localsIndexRule = applyWithOverwrite(
+          url('./files/index-for-entity'),
+          [
+            template({
+              locals: locals_1.map((local_2) => ({
+                ...local_2,
+                title: strings.titlize(local_2.title),
+              })),
+              titles: titlesRef,
+              entities,
+              databaseName: title_1,
+              ...strings,
+            }),
+            move(`${outDir}/${strings.dasherize(title_1)}`),
+          ]
         );
-        cachesRef = includedCaches;
-        const localsPromises = includedCaches.map((cache) =>
-          getlocals({ ...options, pageId: cache.id })
-        );
-        return Promise.all(localsPromises);
+
+        return [...localsRules, localsIndexRule];
       })
-      .then((locals) => {
-        const rules = locals
-          .map(({ title, locals }) => {
-            titlesRef = cachesRef.map(({ title }) => title) as string[];
-            const localsRules = locals.map((local) => {
-              const { title: localTitle, ...rest } = local;
-              const formattedTitle = strings.titlize(localTitle);
-              return applyWithOverwrite(url('./files/all-for-entity'), [
-                template({
-                  title: formattedTitle,
-                  local: rest,
-                  entities,
-                  databaseName: title,
-                  ...strings,
-                }),
-                move(`${outDir}/${strings.dasherize(title)}`),
-              ]);
-            });
-
-            const localsIndexRule = applyWithOverwrite(
-              url('./files/index-for-entity'),
-              [
-                template({
-                  locals: locals.map((local) => ({
-                    ...local,
-                    title: strings.titlize(local.title),
-                  })),
-                  titles: titlesRef,
-                  entities,
-                  databaseName: title,
-                  ...strings,
-                }),
-                move(`${outDir}/${strings.dasherize(title)}`),
-              ]
-            );
-
-            return [...localsRules, localsIndexRule];
-          })
-          .flat();
-
-        const localsRootIndexRule = applyWithOverwrite(url('./files/index'), [
-          template({
-            titles: titlesRef,
-            entities,
-            ...strings,
-          }),
-          move(outDir),
-        ]);
-        return chain([...rules, localsRootIndexRule]);
-      });
+      .flat();
+    const localsRootIndexRule = applyWithOverwrite(url('./files/index'), [
+      template({
+        titles: titlesRef,
+        entities,
+        ...strings,
+      }),
+      move(outDir),
+    ]);
+    return chain([...rules, localsRootIndexRule]);
+  };
 }
