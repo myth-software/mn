d464ad703bcbc554823ca58f3e06061bf93f2718
Peter B Smith <1965474+peterbsmyth@users.noreply.github.com>
2023-07-18 11:10:12 -0700

    camelize express controller

--- packages/schematics/src/factories/express/files/routers/__title@dasherize__.router.ts
+++ packages/schematics/src/factories/express/files/routers/__title@dasherize__.router.ts
@@ -4,7 +4,7 @@
 
 import express, { Request, Response } from 'express';
 
-<%= classify(title) %>Cmport controller from '../controllers/<%= dasherize(title) %>.controller';
+import <%= camelize(title) %>Controller from '../controllers/<%= dasherize(title) %>.controller';
 
 import middleware from '../middleware';
 
@@ -12,12 +12,12 @@ export const <%= camelize(title) %>Router = express.Router();
 
 <%= camelize(title) %>Router.use(middleware.router);
 
-<%= camelize(title) %>Router.get('/', middleware.query, <%= classify(title) %>Controller.query);
+<%= camelize(title) %>Router.get('/', middleware.query, <%= camelize(title) %>Controller.query);
 
-<%= camelize(title) %>Router.get('/:id', middleware.getById, <%= classify(title) %>Controller.getById);
+<%= camelize(title) %>Router.get('/:id', middleware.getById, <%= camelize(title) %>Controller.getById);
 
-<%= camelize(title) %>Router.post('/', middleware.create, <%= classify(title) %>Controller.create);
+<%= camelize(title) %>Router.post('/', middleware.create, <%= camelize(title) %>Controller.create);
 
-<%= camelize(title) %>Router.patch('/:id',  middleware.updateById, <%= classify(title) %>Controller.updateById);
+<%= camelize(title) %>Router.patch('/:id',  middleware.updateById, <%= camelize(title) %>Controller.updateById);
 
-<%= camelize(title) %>Router.delete('/:id', middleware.deleteById, <%= classify(title) %>Controller.deleteById);
+<%= camelize(title) %>Router.delete('/:id', middleware.deleteById, <%= camelize(title) %>Controller.deleteById);
