1478fb6ae7a11e0a5ab03ee99e530c92788c17dd
Peter B Smith <hello@peterbsmith.com>
2023-07-22 11:16:36 -0700

    display userColumn and accessorProperty correctly

--- packages/schematics/src/factories/express/files/controllers-eject/__title@dasherize__.controller.ts
+++ packages/schematics/src/factories/express/files/controllers-eject/__title@dasherize__.controller.ts
@@ -9,10 +9,10 @@ async function query(req: Request, res: Response) {
   const principal = res.locals.principal;
   const principalFilter =
     principal.role === 'client'
-      ? { property: '<% options.userColumn %>', relation: { contains: principal.id } }
+      ? { property: '<%= options.userColumn %>', relation: { contains: principal.id } }
       : {
-          property: '<% options.accessorProperty %>',
-          relation: { contains: principal.<% options.accessorProperty %> },
+          property: '<%= options.accessorProperty %>',
+          relation: { contains: principal.<%= options.accessorProperty %> },
         };
   const filter = where?.filter
     ? { and: [where.filter, principalFilter] }
@@ -36,11 +36,11 @@ async function getById(req: Request, res: Response) {
 }
 async function create(req: Request, res: Response) {
   try {
-    const { id: <% options.userColumn %>, <% options.accessorProperty %> } = res.locals.principal;
+    const { id: <%= options.userColumn %>, <%= options.accessorProperty %> } = res.locals.principal;
     const item: New<%= classify(title) %> = req.body;
     const newItem = await client.<%= camelize(title) %>.create({
-      <% options.userColumn %>: [<% options.userColumn %>],
-      <% options.accessorProperty %>: [<% options.accessorProperty %>],
+      <%= options.userColumn %>: [<%= options.userColumn %>],
+      <%= options.accessorProperty %>: [<%= options.accessorProperty %>],
       ...item,
     });
     res.status(201).json(newItem);
