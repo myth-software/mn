9537e6a1cc183691f92cdfb2f83046887f93c1ae
Peter B Smith <hello@peterbsmith.com>
2023-11-18 09:04:30 -0500

    feat(website): creates website

---
+++ packages/website/server.ts
@@ -0,0 +1,17 @@
+/**
+ * This is not a production server yet!
+ * This is only a minimal backend to get started.
+ */
+
+import express from 'express';
+import * as path from 'path';
+
+const app = express();
+
+app.use('/', express.static(path.join(__dirname, '')));
+
+const port = process.env.PORT || 3333;
+const server = app.listen(port, () => {
+  console.log(`Listening at http://localhost:${port}/`);
+});
+server.on('error', console.error);
