59a5518eca431f6f4470074d02747788864a6def
Peter B Smith <hello@peterbsmith.com>
2024-04-09 13:43:42 -0700

    moves types private

--- packages/types/README.md
+++
@@ -1,3 +0,0 @@
-# Installation
-
-> `npm install --save @mountnotion/types`
--- packages/types/file.d.ts
+++
@@ -1,6 +0,0 @@
-export declare type EncodingFile = {
-  column: string;
-  id: string;
-  ext: string;
-  blurhash?: string;
-};
